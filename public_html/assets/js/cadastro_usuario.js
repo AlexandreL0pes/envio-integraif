1b624078d454f6a4972fb5c52709c61955f5ddae
Alexandre <alexandre9999lopes@gmail.com>
2019-09-24 15:06:16 -0300

    Redirecionamento pós cadastro

--- public_html/assets/js/cadastro_usuario.js
+++ public_html/assets/js/cadastro_usuario.js
@@ -3,7 +3,7 @@ let construct = () => {
 };
 
 const eventos = () => {
-    
+
     $('#formulario').on('submit', function (e) {
         e.preventDefault();
         let nome = $('#nome').val(),
@@ -12,12 +12,12 @@ const eventos = () => {
             senha = $('#senha').val(),
             conf_senha = $('#confirm_senha').val(),
             telefone = $('#telefone').val(),
-            usuario_id  = $('#usuario_id').val();
+            usuario_id = $('#usuario_id').val();
 
         if (nome !== "" &&
             cpf !== "" &&
             email !== "" &&
-            senha == conf_senha 
+            senha == conf_senha
         ) {
             let dados = {
                 nome: nome,
@@ -27,14 +27,9 @@ const eventos = () => {
                 senha: senha,
             };
 
-            if(usuario_id>0){
-                dados.usuario_id=usuario_id;
-                delete dados.senha;
-                dados.acao = "Usuarios/atualizarDados";
-            }else{
-                dados.acao = "Usuarios/cadastrar";
-            }
-    
+            dados.acao = "Usuarios/cadastrar";
+
+
             $.ajax({
                 url: baseUrl,
                 type: "POST",
@@ -43,10 +38,9 @@ const eventos = () => {
                 async: true,
                 success: function (res) {
                     if (res) {
-                        // console.log(res);
+                        console.log(res);
                         // $('#msg_sucesso').toast('show'); // Para aparecer a mensagem de sucesso
-                        // alert("Cadastrou");
-                        window.location="trabalhos.php";
+                        window.location.href = "trabalhos.php";
                         $('#formulario').each(function () {
                             this.reset(); // Pra limpar o formulário
                         });
@@ -59,7 +53,7 @@ const eventos = () => {
                     console.log(request, status, str_error)
                 }
             });
-        }else{
+        } else {
             $('#msg_alerta').toast('show');
         }
     });
